af84ce88638c7c7af3e78d57ac88c424da989345
Fahad Safdar <iFahadislive@gmail.com>
2024-09-16 23:35:29 +0500

    Animated Align Page in Completed with oggu is chasing Cockroach

--- lib/animations/animated_align/animated_align_page.dart
+++ lib/animations/animated_align/animated_align_page.dart
@@ -9,7 +9,7 @@ class AnimatedAlignPage extends StatefulWidget {
 }
 
 class _AnimatedAlignPageState extends State<AnimatedAlignPage> {
-   int _cockroach = 0;
+  int _cockroach = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,29 +19,35 @@ class _AnimatedAlignPageState extends State<AnimatedAlignPage> {
       body: Stack(
         children: [
           AnimatedAlign(
+            alignment: cockroachAnimation(_cockroach+1),
+            duration: const Duration(milliseconds: 100),
+            child: Container(
+                height: 150,
+                width: 150,
+                color: Colors.transparent,
+                child: Image.asset('assets/oggyAndCockroach/cockroach.png')),
+          ),
+          AnimatedAlign(
             alignment: cockroachAnimation(_cockroach),
             duration: const Duration(milliseconds: 100),
-            child: GestureDetector(
-
-              onDoubleTap: (){
-                setState(() {
-                  _cockroach = _cockroach -1;
-                });
-              },
-
-                onTap: (){
-                  setState(() {
-                    _cockroach = _cockroach +1;
-                  });
-                },
-
-                child: Container(
-                    height: 150,
-                    width: 150,
-                    color: Colors.transparent,
-                    child: Image.asset('assets/oggyAndCockroach/cockroach.png'))),
+            child: Container(
+                height: 150,
+                width: 150,
+                color: Colors.transparent,
+                child: Image.asset('assets/oggyAndCockroach/oggy.png')),
           )
         ],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        hoverElevation: 40,
+        onPressed: (){
+         setState(() {
+           _cockroach = _cockroach +1;
+         });
+        },
+        child: const Icon(Icons.animation_outlined, color: Colors.white,),
       ),
     );
   }
@@ -49,29 +55,26 @@ class _AnimatedAlignPageState extends State<AnimatedAlignPage> {
   Alignment cockroachAnimation(int alignment) {
     switch (alignment) {
       case 1:
-        return Alignment.center;
-
-      case 2:
         return Alignment.topLeft;
-
+      case 2:
+        return Alignment.topCenter;
       case 3:
         return Alignment.topRight;
-
       case 4:
-        return Alignment.bottomCenter;
-
+        return Alignment.centerLeft;
       case 5:
-        return Alignment.bottomRight;
-
+        return Alignment.center;
       case 6:
-        return Alignment.topCenter;
-
+        return Alignment.centerRight;
       case 7:
         return Alignment.bottomLeft;
-
-      default: _cockroach = 0;
-
-      return Alignment.center;
+      case 8:
+        return Alignment.bottomCenter;
+      case 9:
+        return Alignment.bottomRight;
+      default:
+        _cockroach = 0;
+        return Alignment.center;
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class FlutterAnimation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: AnimatedAlignPage(),
     );
   }
